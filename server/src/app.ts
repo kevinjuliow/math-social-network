807e9e9be4c9fbb6755d12b1c7002c887c1544fc
Kevin Julio <kevjuliow@gmail.com>
2026-01-22 09:32:25 +0700

    added testing route to test connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,6 +9,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    message: 'Server connected',
+  })
+})
+
 app.use('/api', authRoutes)
 app.use('/api', postRoutes)
 
